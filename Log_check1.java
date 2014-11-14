136b731a01aec274e4ea17ec609001ad31733438
E11029 <kbalakayan@gmail.com>
2014-11-14 15:31:44 +0530

    i add the block and and unblock part.. and this one work  mode  sudo passwd  nopasswd mode use %sudo	ALL=(ALL:ALL) ALL kayanpc ALL=(ALL) NOPASSWD: ALL

---
+++ Log_check1.java
@@ -0,0 +1,124 @@
+//package demo;
+
+
+
+
+
+import java.io.*;
+
+import java.util.Hashtable;
+
+import java.util.regex.Matcher;
+
+import java.util.regex.Pattern;
+
+//This is the main class file
+
+public class Log_check {
+
+//public static String passwd;
+
+	public static void main (String[] args){
+
+ 
+
+		String logFile = "auth (1).log";
+
+		try {
+
+			//make the reading of the file continue until specified otherwise
+
+			FileReader flr = new FileReader(logFile);
+
+			BufferedReader rd = new BufferedReader(flr);
+
+			String line = null;
+
+			// This is placed here only temporally we want refresh the table in a given interval
+
+			Hashtable<String,log_line> f_ips = new Hashtable<String,log_line>();
+
+
+
+		while (true){
+
+			if ((line = rd.readLine()) == null) {
+
+			try {
+
+				Thread.sleep(1000);
+
+				} catch (InterruptedException e) {
+
+					e.printStackTrace();
+
+					}
+
+			}else{
+
+				if (line.contains("Invalid") || line.contains("PAM")){
+
+			//This will extract the ip addressese
+
+			Pattern p = Pattern.compile("(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+
+			Matcher m = p.matcher(line);
+
+			if(m.find()) {
+
+			//System.out.println(m.group(0));
+
+			String temp = m.group(0);
+
+			//Simplify the log_line class when using hashtables things are much easier
+
+			if(f_ips.containsKey(temp)){
+
+				f_ips.get(temp).check_ip(temp);
+
+			if(f_ips.get(temp).check_attack()){
+
+				f_ips.get(temp).block_ip();
+
+			}
+
+			}else{
+
+				log_line temp_line = new log_line(temp);
+
+				f_ips.put(temp, temp_line);
+
+				}
+
+			}
+
+			}
+
+			}
+
+			}
+
+			} catch (FileNotFoundException e) {
+
+				System.out.println("LogFileNotFound");
+
+				e.printStackTrace();
+
+				System.exit(-1);
+
+			} catch (IOException r) {
+
+				System.out.println("Error");
+
+				r.printStackTrace();
+
+				System.exit(-1);
+
+			}
+
+		}
+
+}
+
+
+
